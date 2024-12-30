0452e6d1f6441ca11acb319b0b44eef7a43f87b5
Bablu kumar <bk559722@gmail.com>
2024-12-30 20:07:37 +0530

    add frontend url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    manifest: true, // Ensure build manifest is generated
+  },
+  server: {
+    proxy: {
+      "/api": { // Proxy any request starting with `/api`
+        target: "https://mern-webapp-api.onrender.com/" || "http://localhost:5000", // Use env variable or fallback to localhost
+        changeOrigin: true, // Change the origin header to the target URL
+        secure: false, // Allow self-signed SSL certificates
+        rewrite: (path) => path.replace(/^\/api/, ""), // Remove `/api` prefix before forwarding to the target
+      },
+     
+    },
+  },
+});
